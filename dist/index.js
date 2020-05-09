d6f43fa990d260c613af9e0c7ce32f1657c84131
Michal Szczepanski <michal@vane.pl>
2020-05-09 18:49:48 +0200

    Fix adding .js to imports

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@ const InitModule = (name) => {
   const instantiate = System.constructor.prototype.instantiate;
   System.constructor.prototype.instantiate = function (url, parentUrl) {
     // console.log(url, parentUrl);
-    if (url.indexOf('.') < 0) {
+    if (!url.endsWith('.js')) {
       url += '.js'
     }
     return Promise.resolve(instantiate.call(this, url, parentUrl));
